eb591f585358eb17e62b8b6d486694ea29d4cd4b
OceanDevsInPython <matthew71671@gmail.com>
2025-03-12 23:00:04 +0000

    Add files via upload

---
+++ sigma.py
@@ -0,0 +1,125 @@
+Users_Name = input("Hi whats your name? ")
+
+print("Hello, " + Users_Name)
+
+Users_Age = input("How old are you, " + Users_Name + "? ")
+
+print("So, Your name is " + Users_Name + " and you are " + Users_Age + " years old? \n")
+
+print("Nice to meet you, " + Users_Name + " ")
+
+Users_City = input("Where are you from, " + Users_Name + " ")
+
+print("Oh, " + Users_City + " is a nice place!")
+
+print("Are you familiar with your surroundings. What are they, " + Users_Name)
+
+Surroundings = input("")
+
+print("Are you sure?")
+
+input("")
+
+print("Okay, I have a question " + Users_Name)
+print("Are you lying to me about that you are " + Users_Age + " Years Old?")
+
+input("")
+print("Okay, If you say so...")
+
+Object = input("Look around you. What do you see? Name a object ")
+
+
+print("Wow, " + Object + " Thats a nice choice.")
+
+input("Have you used it in the last 24 hours? ")
+
+print("Lets get this straight! \n")
+print("Your name: " + Users_Name)
+print("Your age: " + Users_Age)
+print("You are from: " + Users_City)
+print("And you have a " + Object + " next to you?")
+Answer = input("Is this correct? ")
+print(Answer)
+print(Users_Age)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Users_Name)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print(Answer)
+print("You arent safe..")
+
+print(Users_Name + ", Can you rethink your decisions")
+
+input("")
+
+print("Do i scare you.")
+print("Can i come visit you in " + Users_City)
+
+input("Check your surroundings again. is the answer still " + Surroundings + " ")
+
+
+print("I will ask you once again, " + Users_Name + " Are you being truthfull this survey?")
+questionsx = input("")
+
+print("You answered " + questionsx + " Is this your final answer?")
+input("")
+
+print("okay. if you say so....")
+
+
+
